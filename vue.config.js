b9ed53ce30dc6d9894001c580e3c729b9e8e8669
Dinie Syahirah <dinie.syahirah@fullode.com>
2023-10-11 12:17:13 +0800

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/Dinie97/rest-countries/",
+  publicPath: "/rest-countries/",
 });
